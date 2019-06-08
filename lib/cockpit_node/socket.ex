862a2802a70e4efb232c6bb15d030b3d8a4bf1ff
Dreae <dreae@dreae.onl>
2019-06-08 16:57:33 -0400

    Send signed server_id to daemon

--- lib/cockpit_node/socket.ex
+++ lib/cockpit_node/socket.ex
@@ -1,5 +1,6 @@
 defmodule CockpitNode.Socket do
     use GenServer, restart: :permanent
+    require Logger
 
     def start_link(_) do
         config = Application.get_env(:cockpit_node, CockpitNode.Socket)
@@ -9,24 +10,37 @@ defmodule CockpitNode.Socket do
     def init(config) do
         port = Keyword.get(config, :port)
         address = to_charlist Keyword.get(config, :address)
-        :timer.send_after(3000, {:connect, address, port})
+        server_id = Keyword.get(config, :server_id)
+        api_key = Keyword.get(config, :key)
+        :timer.send_after(3000, {:connect, address, port, server_id, api_key})
     end
 
-    def handle_info({:connect, address, port}, _state) do
+    def handle_info({:connect, address, port, server_id, api_key}, _state) do
         {:ok, socket} = :gen_tcp.connect(address, port, [:binary, packet: 2, active: :once])
-        {:noreply, socket}
+        signature = :crypto.hmac(:sha512, <<server_id::big-unsigned-32>>, to_charlist(api_key), 16)
+
+        :gen_tcp.send(socket, <<server_id::big-unsigned-32, signature::binary>>)
+        :timer.send_interval(5000, :ping)
+        {:noreply, %{server_id: server_id, key: api_key, socket: socket}}
     end
 
-    def handle_info({:tcp, socket, _data}, _state) do
+    def handle_info(:ping, %{socket: socket} = state) do
+        Logger.debug("Sending ping to daemon")
+        :gen_tcp.send(socket, 'ping')
+
+        {:noreply, state}
+    end
+
+    def handle_info({:tcp, socket, _data}, state) do
         :inet.setopts(socket, [active: :once])
-        {:noreply, socket}
+        {:noreply, state}
     end
 
-    def handle_info({:tcp_closed, socket}, _state) do
-        {:stop, :socket_closed, socket}
+    def handle_info({:tcp_closed, _socket}, state) do
+        {:stop, :socket_closed, state}
     end
 
-    def handle_info({:tcp_error, socket}, _state) do
-        {:stop, :socket_closed, socket}
+    def handle_info({:tcp_error, _socket}, state) do
+        {:stop, :socket_closed, state}
     end
 end
